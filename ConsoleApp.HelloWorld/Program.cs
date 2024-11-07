07fe4e4fccb72c6c16c18376aed1b6afa78dc521
Jesse <174287334+Hjesse23@users.noreply.github.com>
2024-11-06 17:04:20 -0800

    Initial commit

---
+++ ConsoleApp.HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// WriteLine is a method that takes a block of text and prints it to the screen.
+Console.WriteLine("Hello, World! My name is Jesse Heinrich.");
+Console.WriteLine("I am from the United States");
+
+// Write is a method that takes a block of text and prints it to the screen.
+// It does not break the line after it prints the text.
+Console.Write("Line 1");
+Console.Write("Line 2");
+
+
+
